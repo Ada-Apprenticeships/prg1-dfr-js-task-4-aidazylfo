cfd699544fc96ce3b27199172d42cc91c2db349d
aidazylfo <aida.zylfo@ada.ac.uk>
2024-10-28 12:05:21 +0000

    task 1 done

--- dfr.js
+++ dfr.js
@@ -1,57 +1,56 @@
 const fs = require('fs'); 
-
-
 function fileExists(filename) {
   // returns true or false
 }
-
-
+// Task 1
 function validNumber(value) { // value can be string or numeric
- // returns a boolean 
-}
+// returns a boolean 
+{ 
+  return typeof value === 'number' && isFinite(value); // a value is equal to a number or a realistic value 
+} }
 
-
+// Task 2
 function dataDimensions(dataframe) {
   // returns a list [rows (int), cols (int)]
 }
 
-
+// Task 3
 function calculateMean(dataset) {
   // returns a float or false
   
 }
 
-
+// Task 4
 function findTotal(dataset) {
   // returns float or false
   
 } 
 
-
+// Task 5
 function convertToFloat(dataframe, col){ //dataframe, integer
   // returns an integer, which is the number that were  converted to floats.
   
 }
 
-
+// Task 6
 function flatten(dataframe) {
   // returns a dataset (a flattened dataframe)
   
 }
 
-
+// Task 7
 function loadCSV(csvFile, ignorerows, ignorecols) {  // string, dataset, dataset
   // returns a list comprising of [dataframe, rows (integer), cols (integer)]
 
 }
 
-
+// Task 8 
 function calculateMedian(dataset) {
   // return float or false 
   
 }
 
-
+// Task 9
 function createSlice(dataframe, colindex, colpattern, exportcols = []) { // dataframe, integer, string/numeric, dataset
   // returns a dataframe
   
